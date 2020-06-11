e99fbf402e7cf3fc0d0a1fbe6247c4a5220bea6a
Wallace Frota <frotadev@gmail.com>
2020-06-11 10:08:03 -0300

    Adicionado arquivos de configuração

---
+++ src/config/StatusBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+// estilo statusbar
+const Status = () => {
+    return (
+        <StatusBar barStyle="light-content" backgroundColor="#bf8106"/>
+    )
+};
+
+export default Status;
